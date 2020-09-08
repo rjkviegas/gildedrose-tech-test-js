f2913260fcf2e7b64e379bc34d594c73368170fb
Ryan Viegas <rjkviegas@gmail.com>
2020-09-08 19:21:11 +0100

    Test Shop #isNormalItem returns false for special items

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -7,7 +7,25 @@ describe('Shop', function() {
     it('returns true for normal item', function() {
       const shop = new Shop();
       const itemDouble = jasmine.createSpyObj('itemDouble', {}, {name: 'normalItem'});
-      expect(shop.isNormalItem(itemDouble)).toEqual(true)
+      expect(shop.isNormalItem(itemDouble)).toBeTrue();
+    });
+
+    it('returns false for Sulfuras', function() {
+      const shop = new Shop();
+      const itemDouble = jasmine.createSpyObj('itemDouble', {}, {name: 'Sulfuras, Hand of Ragnaros'});
+      expect(shop.isNormalItem(itemDouble)).toBeFalse()
+    });
+
+    it('returns false for Aged Brie', function() {
+      const shop = new Shop();
+      const itemDouble = jasmine.createSpyObj('itemDouble', {}, {name: 'Aged Brie'});
+      expect(shop.isNormalItem(itemDouble)).toBeFalse()
+    });
+
+    it('returns false for Backstage passes', function() {
+      const shop = new Shop();
+      const itemDouble = jasmine.createSpyObj('itemDouble', {}, {name: 'Backstage passes to a TAFKAL80ETC concert'});
+      expect(shop.isNormalItem(itemDouble)).toBeFalse()
     });
   });
 });
