922005797949ff1b91215fa38421a0c07b1b256d
Ryan Viegas <rjkviegas@gmail.com>
2020-09-08 12:31:10 +0100

    Test Gilded Rose #updateQuality normal item

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,15 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+  describe("#updateQuality", function() {
+
+    it("normal item", function() {
+      const gildedRose = new Shop([ new Item("+5 Dexterity Vest", 10, 20) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toEqual("+5 Dexterity Vest");
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(19);
+    });
   });
 
 });
