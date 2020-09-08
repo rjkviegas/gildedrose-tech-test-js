4fa9d0724fb8beda3519b58a6c1bf53b5ecc6f60
Ryan Viegas <rjkviegas@gmail.com>
2020-09-08 14:51:04 +0100

    refactor guard condition for sulfaras

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,46 +12,44 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach( function(item) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality -= 1;
-          }
-        }
+      if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        return item
       } else {
-        if (item.quality < 50) {
-          item.quality += 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality += 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality += 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn -= 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality -= 1;
-              }
-            }
-          } else {
-            item.quality = 0;
+        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0) {
+              item.quality -= 1;
           }
         } else {
           if (item.quality < 50) {
             item.quality += 1;
+            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+              if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                  item.quality += 1;
+                }
+              }
+              if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                  item.quality += 1;
+                }
+              }
+            }
+          }
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+          if (item.name != 'Aged Brie') {
+            if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+              if (item.quality > 0) {
+                  item.quality -= 1;
+              }
+            } else {
+              item.quality = 0;
+            }
+          } else {
+            if (item.quality < 50) {
+              item.quality += 1;
+            }
           }
         }
       }
