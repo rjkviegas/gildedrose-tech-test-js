aed7c34575e2acd247f1eb3502beb3a0b115f77a
Ryan Viegas <rjkviegas@gmail.com>
2020-09-08 13:45:45 +0100

    Test Gilded Rose #updateQuality does not reduce quality of normal item below 0

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,12 +3,24 @@ describe("Gilded Rose", function() {
 
   describe("#updateQuality", function() {
 
-    it("normal item", function() {
+    it("reduces sellIn and quality of a normal item", function() {
       const gildedRose = new Shop([ new Item("+5 Dexterity Vest", 10, 20) ]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toEqual("+5 Dexterity Vest");
       expect(items[0].sellIn).toEqual(9);
+      expect(items[0].sellIn).not.toEqual(10);
       expect(items[0].quality).toEqual(19);
+      expect(items[0].quality).not.toEqual(20);
+    });
+
+    it("does not reduce quality of normal item below 0", function() {
+      const gildedRose = new Shop([ new Item("+5 Dexterity Vest", 10, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toEqual("+5 Dexterity Vest");
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].sellIn).not.toEqual(10);
+      expect(items[0].quality).toEqual(0);
+      expect(items[0].quality).not.toEqual(-1);
     });
   });
 
