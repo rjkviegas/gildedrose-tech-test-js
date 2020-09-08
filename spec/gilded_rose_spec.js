809eee282cddcb3abc2e1e4ee8d7c25069ffbde1
Ryan Viegas <rjkviegas@gmail.com>
2020-09-08 13:59:59 +0100

    Test Gilded Rose #updateQuality Aged Brie quality increases as days increase

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -38,6 +38,16 @@ describe("Gilded Rose", function() {
       expect(dayTwoItems[0].quality).toEqual(1);
       expect(dayTwoItems[0].quality).not.toEqual(2);
     });
+
+    it('Aged Brie quality increases as days increase', function() {
+      const gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toEqual('Aged Brie')
+      expect(items[0].sellIn).toEqual(1)
+      expect(items[0].sellIn).not.toEqual(2)
+      expect(items[0].quality).toEqual(1)
+      expect(items[0].quality).not.toEqual(0)
+    });
   });
 
 });
